59cc8fe9071b7c288f6968c4b642c51ef72dc8bb
Andrii Lebediev <andrii.lebediev@bank.gov.ua>
2025-10-15 19:32:30 +0300

    add endpoint /users

--- app/main.py
+++ app/main.py
@@ -9,3 +9,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/users/")
+def create_user(username: str, email: str):
+ return {"username": username, "email": email, "id": 1}
